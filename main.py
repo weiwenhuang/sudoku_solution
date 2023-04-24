9cca758fde3a5658fab6bd744d094fd7989af70d
Weiwen Huang <56329298+weiwenhuang@users.noreply.github.com>
2023-04-24 15:54:17 -0400

    11

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     csp4 = make_csp_for_4()
     #revise(make_csp_for_4(),'C12','C11')
     ac_3(csp4)
+    backtraking_search(csp4)
 
 def revise(csp,x1,x2):
     revise = False
@@ -48,7 +49,9 @@ def ac_3(csp):
                     if istr not in queue_view:
                         queue.append(i)
                         queue_view.add(istr)
-    print(minimum_remaining_values(csp,'C11','C12'))
+    if not check(csp):
+        return False
+    return True
     
 def make_csp_for_4():
     variable = ['C11','C12','C13','C14','C21','C22','C23','C24','C31','C32','C33','C34','C41','C42','C43','C44']
@@ -64,6 +67,43 @@ def minimum_remaining_values(csp,x1,x2):
         return x1
     else:
         return x2
+
+def backtraking_search(csp):
+    def backtraking(csp):
+        if not unassign_array:
+            print('FOUND IT')
+            return csp.domain
+        tem = unassign_array.pop(0)
+        print(tem)
+        
+    assign_array , unassign_array = findassign(csp.domain)
+    print(unassign_array)
+    backtraking(csp)
+
+def findassign(domain):
+    assign_array = []
+    unassign_array = []
+    for i in domain:
+        if len(domain[i]) == 1:
+            assign_array.append(i)
+        else:
+            unassign_array.append(i)
+    return assign_array,unassign_array
+
+
+def assigned(domain):
+    for i in domain:
+        if len(domain[i]) != 1:
+            return False
+    return True
+
+def check(csp): #check is any val with not domain
+    domain = csp.domain
+    for i in domain:
+        if len(domain[i]) == 0:
+            print("no solution")
+            return False
+    return True
 
 if __name__ == '__main__':
     main()
